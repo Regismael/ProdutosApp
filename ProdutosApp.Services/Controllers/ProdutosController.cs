1ee47e3e263173da5fd430c3a17ede326920436a
regismael <ismael.l.regis@gmail.com>
2023-07-28 10:06:33 -0300

    first commit

---
+++ ProdutosApp.Services/Controllers/ProdutosController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProdutosApp.Data.Entities;
+using ProdutosApp.Data.Repositories;
+using ProdutosApp.Services.Models;
+using System.Net.NetworkInformation;
+
+namespace ProdutosApp.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        /// <summary>
+        /// Método para serviço de cadastro de produtos:
+        /// POST /api/produtos
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(ProdutosGetModel), 200)]
+        public IActionResult Post([FromBody] ProdutosPostModel model)
+        {
+            try
+            {
+                //criando um objeto da classe Produto
+                var produto = new Produto
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = model.Nome,
+                    Preco = model.Preco,
+                    Quantidade = model.Quantidade,
+                    DataHoraCriacao = DateTime.Now,
+                    DataHoraUltimaAlteracao = DateTime.Now
+                };
+
+                //gravar no banco de dados
+                var produtoRepository = new ProdutoRepository();
+                produtoRepository.Add(produto);
+
+                //copiando os dados do Produto para a classe ProdutosGetModel
+                var result = EntityToModel(produto);
+
+                //retornar 200 (OK)
+                return StatusCode(200, result);
+            }
+            catch (Exception e)
+            {
+                //retornar erro 500 (INTERNAL SERVER ERROR)
+                return StatusCode(500, new { e.Message });
+            }
+        }
+
+        /// <summary>
+        /// Método para serviço de edição de produtos:
+        /// PUT /api/produtos
+        /// </summary>
+        [HttpPut]
+        [ProducesResponseType(typeof(ProdutosGetModel), 200)]
+        public IActionResult Put([FromBody] ProdutosPutModel model)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Método para serviço de exclusão de produtos:
+        /// DELETE /api/produtos
+        /// </summary>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(ProdutosGetModel), 200)]
+        public IActionResult Delete(Guid? id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Método para serviço de consulta de produtos:
+        /// GET /api/produtos
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<ProdutosGetModel>), 200)]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ProdutosGetModel), 200)]
+        public IActionResult GetById(Guid? id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Método auxiliar para copiar os dados de um objeto da classe Produto
+        /// para um objeto da classe ProdutosGetModel
+        /// </summary>
+        private ProdutosGetModel EntityToModel(Produto produto)
+        {
+            //copiando os dados do Produto para a classe ProdutosGetModel
+            return new ProdutosGetModel
+            {
+                Id = produto.Id,
+                Nome = produto.Nome,
+                Preco = produto.Preco,
+                Quantidade = produto.Quantidade,
+                Total = produto.Preco * produto.Quantidade,
+                DataHoraCriacao = produto.DataHoraCriacao,
+                DataHoraUltimaAlteracao = produto.DataHoraUltimaAlteracao
+            };
+        }
+    }
+}
+
+
+
---
+++ ProdutosApp.Services/Models/ProdutosPostModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProdutosApp.Services.Models
+{
+    /// <summary>
+    /// Modelo de dados para cadastro de produto na API
+    /// </summary>
+    public class ProdutosPostModel
+   {
+    [MinLength(8, ErrorMessage = "Por favor,informe no mínimo {1}caracteres.")]
+    [MaxLength(150, ErrorMessage = "Por favor,informe no máximo {1} caracteres.")]
+    [Required(ErrorMessage = "Por favor, informe o nome do produto.")]
+    public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe o preço do produto.")]
+    public decimal? Preco { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe a quantidade do produto.")]
+    public int? Quantidade { get; set; }
+   }
+}
